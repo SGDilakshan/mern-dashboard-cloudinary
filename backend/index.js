e48696ee51847f16d7322f6e0346d64371e8aedb
SGDilakshan <sgd.dilakshan@gmail.com>
2025-08-10 11:25:34 +0530

    setup completed

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
-// index.js
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
 const app = express();
-const PORT = 3000;
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello, Node.js World!");
-});
+// Routes
+// app.use("/api/components", require("./routes/componentRoutes"));
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
