cb0cb8f44f5e8b977f11868832099301e435ed2d
SGDilakshan <sgd.dilakshan@gmail.com>
2025-08-11 00:36:44 +0530

    update the database.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
+    console.log("✅ MongoDB connected");
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed", error);
+    console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
+
