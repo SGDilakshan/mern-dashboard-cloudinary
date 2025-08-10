281ed8af683b3f1e2b63697469cca22a00f4d152
SGDilakshan <sgd.dilakshan@gmail.com>
2025-08-11 00:36:32 +0530

    update the index.js

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,4 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./config/db");
+const lib = require("./lib");
 
-dotenv.config();
-connectDB();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Routes
-// app.use("/api/components", require("./routes/componentRoutes"));
-
-app.use("/api/header", require("./route/headerRoutes.route.js"));
-app.use("/api/navbar", require("./route/navbarRoutes.route.js"));
-app.use("/api/footer", require("./route/footerRoutes.route.js"));
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+module.exports = lib.default;
+Object.assign(module.exports, lib);
